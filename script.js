a47dc4d63ef117c979014c840b2eb9d2cda2b7d9
vishalchoudhary9 <vishalchoudhary9770@gmail.com>
2024-11-27 16:52:40 +0530

    initial files

---
+++ script.js
@@ -0,0 +1,20 @@
+const addFriendButton = document.querySelector('.add-friend');
+const relation = document.querySelector('.relation');
+
+addFriendButton.addEventListener('click', () => {
+    if (addFriendButton.innerHTML === '<i class="ri-subtract-fill"></i> Remove Friend') {
+        addFriendButton.innerHTML = 'Removed';
+        setTimeout(() => {
+        addFriendButton.innerHTML = '<i class="ri-add-line"></i> Add Friend';
+        relation.innerHTML = 'Stranger';
+        relation.style.color = 'red';
+        }, 250);
+    } else {
+        addFriendButton.innerHTML = '<i class="ri-check-line"></i> Added';
+        setTimeout(() => {
+            addFriendButton.innerHTML = '<i class="ri-subtract-fill"></i> Remove Friend';
+            relation.innerHTML = 'Friend';
+            relation.style.color = 'green';
+        }, 250);
+    }
+});
