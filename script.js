f1634e0c9d2401238930dff73197854950b62373
vishalchoudhary9 <vishalchoudhary9770@gmail.com>
2024-11-27 17:20:02 +0530

    inital files

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const addFriendButton = document.querySelector('.add-friend');
-const relation = document.querySelector('.relation');
+const addFriendButton = document.querySelector(".add-friend");
+const relation = document.querySelector(".relation");
 
-addFriendButton.addEventListener('click', () => {
-    if (addFriendButton.innerHTML === '<i class="ri-subtract-fill"></i> Remove Friend') {
-        addFriendButton.innerHTML = 'Removed';
-        setTimeout(() => {
-        addFriendButton.innerHTML = '<i class="ri-add-line"></i> Add Friend';
-        relation.innerHTML = 'Stranger';
-        relation.style.color = 'red';
-        }, 250);
-    } else {
-        addFriendButton.innerHTML = '<i class="ri-check-line"></i> Added';
-        setTimeout(() => {
-            addFriendButton.innerHTML = '<i class="ri-subtract-fill"></i> Remove Friend';
-            relation.innerHTML = 'Friend';
-            relation.style.color = 'green';
-        }, 250);
-    }
+addFriendButton.addEventListener("click", () => {
+  if (
+    addFriendButton.innerHTML ===
+    '<i class="ri-subtract-fill"></i> Remove Friend'
+  ) {
+    addFriendButton.innerHTML = "Removed";
+    setTimeout(() => {
+      addFriendButton.innerHTML = '<i class="ri-add-line"></i> Add Friend';
+      relation.innerHTML = "Stranger";
+      relation.style.color = "red";
+    }, 250);
+  } else {
+    addFriendButton.innerHTML = '<i class="ri-check-line"></i> Added';
+    setTimeout(() => {
+      addFriendButton.innerHTML =
+        '<i class="ri-subtract-fill"></i> Remove Friend';
+      relation.innerHTML = "Friend";
+      relation.style.color = "green";
+    }, 250);
+  }
 });
